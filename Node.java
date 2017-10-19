e2ed44251790ca147929969442eafc98eb4112b3
Lucas <knightfication@gmail.com>
2017-10-19 15:01:45 -0500

    Initial Commit
    Just updating the initial files as they are.

---
+++ Node.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class Node<E> {
+    private static final double P = 0.5;
+    private static final int MAX_LEVELS = 30;
+
+    private E data; // value stored in this element
+    private ArrayList<Node<E>> nextNodes; // ref to next
+
+    public Node(E v)
+    {
+        int height = 1;
+        while (Math.random() < P && height < MAX_LEVELS)
+        {
+            height++;
+        }
+        nextNodes = new ArrayList<Node<E>>(height);
+    }
+
+    public int levels()
+    {
+        return nextNodes.size() - 1;
+    }
+
+    public Node<E> next(int level)
+    {
+        return nextNodes.get(level);
+    }
+
+    public void setNext(int level, Node<E> next)
+    {
+        nextNodes.set(level, next);
+    }
+
+    public E value()
+    {
+        return data;
+    }
+
+    public void setValue(E value)
+    {
+        data = value;
+    }
+}
